826d002343d2f7caf5addf36735a489be518c88c
Luka Camus <luka.camus@epitech.eu>
2023-03-13 10:54:10 +0100

    [MODIFY] nvim config
    added hls support and snippets for haskell
    
    just a header for now

---
+++ dotfiles/config/nvim/lua/lsp/hls.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+M.setup = function(on_attach, capabilities)
+	require("lspconfig").hls.setup({
+		capabilities = capabilities,
+		cmd = { "haskell-language-server-wrapper", "--lsp" },
+		on_attach = on_attach,
+		filetypes = { "haskell", "lhaskell" },
+		settings = {
+			haskell = {
+				formattingProvider = "stylish-haskell",
+				cabalFormattingProvider = "cabalfmt",
+			},
+		},
+	})
+end
+
+return M
